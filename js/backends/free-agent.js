5f54003c6d3b6ebf21d3bed5e1144e05712a9dca
Paul Bailey <paul.m.bailey@gmail.com>
2014-06-11 04:49:10 +0000

    fixes #8

---
+++ js/backends/free-agent.js
@@ -0,0 +1,51 @@
+function LocalFree (name, info, scope, pid) {
+  LocalFS.call(this, name, info, scope, pid);
+  
+  this.cid = 'LocalFree';
+}
+
+LocalFree.prototype = Object.create(LocalFS.prototype);
+
+LocalFree.prototype.open_file = function (fileEntry) {
+  var self = this;
+  
+  fileEntry.file(function (f) {
+    fileEntry.path = "/" + randomString(20) + fileEntry.fullPath;
+    fileEntry.id = self.file_id(fileEntry.path);
+    
+    var reader = new FileReader();
+    reader.onloadend = function (e) {
+      self.scope.rootScope.$emit('addTab', fileEntry, this.result, self, function (t) { t.fileEntry = fileEntry; });
+    };
+    
+    reader.readAsText(f);
+  }, function () { self.scope.rootScope.error_message('Error Opening: ' + fileEntry.name); });
+};
+
+LocalFree.prototype.do_save = function (tab, name, path, text, md5sum, mid, errorHandler) {
+  var self = this;
+  
+  tab.fileEntry.createWriter(function(fileWriter) {
+    var truncated = false;
+    
+    fileWriter.onwriteend = function (e) {
+      if (!truncated) {
+        truncated = true;
+        this.truncate(this.position);
+        return;
+      }
+      
+      tab.saved_md5sum = md5sum;
+      tab.saving = false;
+      self.scope.rootScope.$emit('removeMessage', mid);
+      tab.scope.$apply();
+    };
+    
+    fileWriter.onerror = function (e) {
+      errorHandler();
+    };
+    
+    var blob = new Blob([text], {type: 'text/plain'});
+    fileWriter.write(blob);
+  }, errorHandler);
+};
