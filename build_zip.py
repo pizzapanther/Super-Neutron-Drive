94d952c896a29019bd84ea7e6ce99d3b798ccecb
Paul Bailey <paul.m.bailey@gmail.com>
2014-09-10 14:24:43 +0000

    updated build script

--- build_zip.py
+++ build_zip.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import types
 import subprocess
 
 exclude_files = ()
@@ -28,10 +29,34 @@ if __name__ == "__main__":
       if skip:
         continue
         
-      ziplist += ' ' + path
-      
+      if file == 'manifest.json':
+        fh = open(path, 'r')
+        manifest = json.loads(fh.read())
+        fh.close()
+        
+        remove_perm = []
+        for i in range(0, len(manifest['permissions'])):
+          perm = manifest['permissions'][i]
+          if type(perm) in types.StringTypes:
+            if 'http://' in perm:
+              remove_perm.append(perm)
+              
+        for perm in remove_perm:
+          manifest['permissions'].remove(perm)
+          
+        del manifest['server_url']
+        
+        fh = open('/tmp/manifest.json', 'w')
+        fh.write(json.dumps(manifest, indent=2))
+        fh.close()
+        
+      else:
+        ziplist += ' ' + path
+        
   subprocess.call("rm ../super-neutron.zip", shell=True)
   subprocess.call("zip -r ../super-neutron.zip" + ziplist, shell=True)
+  subprocess.call("zip -r ../super-neutron.zip -j /tmp/manifest.json", shell=True)
+  subprocess.call("rm /tmp/manifest.json", shell=True)
   
   print("\nSo long and thanks for all the fish!")
   
