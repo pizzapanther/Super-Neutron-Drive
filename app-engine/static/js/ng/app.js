795fe8c81d5dafa68e4774fdf0b87024fa096d99
Paul Bailey <paul.m.bailey@gmail.com>
2014-10-24 16:41:58 +0000

    fixes #27

--- app-engine/static/js/ng/app.js
+++ app-engine/static/js/ng/app.js
@@ -6,3 +6,19 @@ var ndrive = angular.module(
     $interpolateProvider.endSymbol(']]');
   }
 );
+
+ndrive.run(function ($rootScope) {
+  $rootScope.menu_on = false;
+  
+  $rootScope.toggle_menu = function () {
+    if ($rootScope.menu_on) {
+      $rootScope.menu_on = false;
+    }
+    
+    else {
+      $rootScope.menu_on = true;
+    }
+    
+    console.log($rootScope.menu_on);
+  };
+});
