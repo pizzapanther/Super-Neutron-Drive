7eb051fd8a0b198561abf4d6d9fdf30b07dc2b90
Paul Bailey <paul.m.bailey@gmail.com>
2014-10-24 21:11:27 +0000

    easter egg

--- app-engine/static/js/ng/app.js
+++ app-engine/static/js/ng/app.js
@@ -28,13 +28,41 @@ ndrive.run(function ($rootScope, $mdDialog) {
       locals: {img: img, title: title}
     });
   };
+  
+  $rootScope.egg_modal = function() {
+    $mdDialog.show({
+      controller: 'EggController',
+      templateUrl: '/static/templates/egg.html'
+    });
+  };
+  
+  $rootScope.k_code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  $rootScope.keys = [];
+  $rootScope.easter_egg = function ($event) {
+    $rootScope.keys.push($event.keyCode);
+    
+    while ($rootScope.keys.length > 10) {
+      $rootScope.keys.shift();
+    }
+    
+    if ($rootScope.keys.length == 10) {
+      if (JSON.stringify($rootScope.keys) == JSON.stringify($rootScope.k_code)) {
+        $rootScope.egg_modal();
+      }
+    }
+  };
 });
 
 ndrive.controller('EnlargeController', function($scope, $mdDialog, img, title) {
   $scope.img = img;
   $scope.title = title;
-  console.log($scope);
   
+  $scope.closeDialog = function() {
+    $mdDialog.hide();
+  };
+});
+
+ndrive.controller('EggController', function($scope, $mdDialog) {
   $scope.closeDialog = function() {
     $mdDialog.hide();
   };
