5598516acc54925147f67d0c9c000e27f65dbd0d
Paul Bailey <paul.m.bailey@gmail.com>
2014-11-04 02:17:44 +0000

    added back click jack

--- server/ndrive/settings/__init__.py
+++ server/ndrive/settings/__init__.py
@@ -99,7 +99,7 @@ MIDDLEWARE_CLASSES = (
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
-  #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'ndrive.urls'
