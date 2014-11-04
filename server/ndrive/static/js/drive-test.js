24b73b917be6dfd00985f61f72e949e1e50b943d
Paul Bailey <paul.m.bailey@gmail.com>
2014-11-04 14:48:01 +0000

    added test

---
+++ server/ndrive/static/js/drive-test.js
@@ -0,0 +1,276 @@
+const boundary = '-------314159265358979323846';
+const delimiter = "\r\n--" + boundary + "\r\n";
+const close_delim = "\r\n--" + boundary + "--";
+
+var Drive = {
+  requests: [],
+  fields: 'id,mimeType,labels,fileExtension,title,webViewLink,properties(key,value),alternateLink,webContentLink'
+};
+
+Drive.list_dir = function (data, callback) {
+  var folderId = data.folderId;
+  
+  var retrievePageOfFiles = function(request, params, result) {
+    request.execute(function(resp) {
+      if (resp) {
+        if (resp.error) {
+          callback({folderId: folderId, result: resp});
+        }
+        
+        else {
+          result = result.concat(resp.items);
+          if (resp.nextPageToken) {
+            params.pageToken = resp.nextPageToken;
+            request = gapi.client.drive.files.list(params);
+            retrievePageOfFiles(request, params, result);
+          }
+          
+          else {
+            callback({folderId: folderId, result: result});
+          }
+        }
+      }
+      
+      else {
+        callback({folderId: folderId, result: result});
+      }
+    });
+  };
+  
+  var params = {
+    q: "'root' in parents",
+    fields: 'items(' + Drive.fields + ')'
+  };
+  if (folderId) {
+    if (folderId === 'sharedWithMe') {
+      params.q = "sharedWithMe";
+    }
+    
+    else {
+      params.q = "'" + folderId + "' in parents";
+    }
+  }
+  
+  var initialRequest = gapi.client.drive.files.list(params);
+  retrievePageOfFiles(initialRequest, params, []);
+};
+
+Drive.open = function (data, callback) {
+  var title = data.title;
+  var fileId = data.fileId;
+  
+  var request = gapi.client.drive.files.get({fileId: fileId});
+  request.execute(function (response) {
+    if (response.downloadUrl) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', response.downloadUrl);
+      xhr.setRequestHeader('Authorization', 'Bearer ' + Neutron.OAuth.access_token);
+      xhr.onload = function() {
+        response.content = xhr.responseText;
+        response.fileId = fileId;
+        callback(response);
+      };
+      xhr.onerror = function() {
+        callback({title: title, fileId: fileId, error: 'Error Opening: ' + title});
+      };
+      xhr.send();
+    }
+    
+    else {
+      if(response.error) {
+        callback({title: title, fileId: fileId, error: response.error});
+      }
+      
+      else {
+        callback({title: title, fileId: fileId, error: 'Can Not Open: ' + title});
+      }
+    }
+  });
+};
+
+Drive.save = function (data, callback) {
+  var base64Data = btoa(unescape(encodeURIComponent(data.text)));
+  var multipartRequestBody =
+    delimiter +
+    'Content-Type: application/json\r\n\r\n' +
+    delimiter +
+    'Content-Type: ' + data.mimeType + '\r\n' +
+    'Content-Transfer-Encoding: base64\r\n' +
+    '\r\n' +
+    base64Data +
+    close_delim;
+    
+  var request = gapi.client.request({
+    'path': '/upload/drive/v2/files/' + data.fileId,
+    'method': 'PUT',
+    'params': {
+      'uploadType': 'multipart',
+      'alt': 'json',
+      'newRevision': 'false',
+      'useContentAsIndexableText': 'true'
+    },
+    'headers': {
+      'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+    },
+    'body': multipartRequestBody});
+    
+  request.execute(function (file) {
+    var response = {fileId: data.fileId}
+    if (file.error) {
+      response.error = file.error;
+    }
+    
+    callback(response);
+  });
+};
+
+Drive.webview = function (data, callback) {
+  var request;
+  
+  if (data.make_public) {
+    var body = {'value': '', 'type': 'anyone', 'role': 'reader'};
+    request = gapi.client.drive.permissions.insert({
+      'fileId': data.fileId,
+      'resource': body
+    });
+    
+    request.execute(function (resp) {
+      Drive.get_file(data.fileId, callback);
+    });
+  }
+  
+  else {
+    request = gapi.client.drive.permissions.list({'fileId': data.fileId});
+    request.execute(function(resp) {
+      Drive.requests = [];
+      for (var i=0; i < resp.items.length; i++) {
+        if (resp.items[i].type == 'anyone') {
+          r = gapi.client.drive.permissions.delete({
+            'fileId': data.fileId,
+            'permissionId': resp.items[i].id
+          });
+          
+          Drive.requests.push(r);
+        }
+      }
+      
+      if (Drive.requests.length > 0) {
+        Drive.do_requests(0, function () {
+          Drive.get_file(data.fileId, callback);
+        });
+      }
+      
+      else {
+        Drive.get_file(data.fileId, callback);
+      }
+    });
+  }
+};
+
+
+Drive.get_file = function (fileId, callback) {
+  var request = gapi.client.drive.files.get({'fileId': fileId, fields: Drive.fields});
+  
+  request.execute(function (resp) {
+    resp.fileId = fileId;
+    callback(resp);
+  });
+};
+
+Drive.do_requests = function (r, done) {
+  Drive.requests[r].execute(function (resp) {
+    r = r + 1;
+    
+    if (r < Drive.requests.length) {
+      Drive.do_requests(r, done);
+    }
+    
+    else {
+      done();
+    }
+  });
+}
+
+Drive.rename = function (data, callback) {
+  var body = {'title': data.new_name};
+  var request = gapi.client.drive.files.patch({
+    'fileId': data.fileId,
+    'resource': body
+  });
+  
+  request.execute(function (resp) {
+    resp.fileId = data.fileId;
+    callback(resp);
+  });
+};
+
+Drive.trash = function (data, callback) {
+  var request = gapi.client.drive.files.trash({
+    'fileId': data.fileId
+  });
+  
+  request.execute(function(resp) {
+    resp.fileId = data.fileId;
+    callback(resp);
+  });
+};
+
+Drive.newfile = function (data, callback) {
+  var metadata = {title: data.name};
+  if (data.parentId) {
+    metadata.parents = [{'id': data.parentId}];
+  }
+  
+  if (data.dir) {
+    metadata.mimeType = "application/vnd.google-apps.folder";
+    
+    gapi.client.drive.files.insert({'resource': metadata}).execute(function (file) {
+      file.parentId = data.parentId;
+      file.dir = true;
+      callback(file);
+    });
+  }
+  
+  else {
+    var base64Data = '';
+    var multipartRequestBody =
+      delimiter +
+      'Content-Type: application/json\r\n\r\n' +
+      JSON.stringify(metadata) +
+      delimiter +
+      'Content-Type: application/octet-stream\r\n' +
+      'Content-Transfer-Encoding: base64\r\n' +
+      '\r\n' +
+      base64Data +
+      close_delim;
+      
+    var request = gapi.client.request({
+      'path': '/upload/drive/v2/files',
+      'method': 'POST',
+      'params': {
+        'uploadType': 'multipart',
+        'useContentAsIndexableText': 'true'
+      },
+      'headers': {
+        'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+      },
+      'body': multipartRequestBody});
+      
+    request.execute(function (file) {
+      file.parentId = data.parentId;
+      callback(file);
+    });
+  }
+};
+
+Drive.share = function (data, callback) {
+  Neutron.share.setItemIds([data.fileId]);
+  Neutron.share.showSettingsDialog();
+  
+  document.querySelector('#sharing-btn').style.display = 'block';
+};
+
+Drive.done_sharing = function () {
+  document.querySelector('#sharing-btn').style.display = 'none';
+  Neutron.parent.postMessage({'task': 'hide-webview', id: Neutron.id}, Neutron.origin);
+};
---
+++ server/ndrive/static/js/neutron-test.js
@@ -0,0 +1,182 @@
+var immediateAuth = false;
+var Neutron = {
+  gdrive_api_loaded: false,
+  authorized: false,
+  authorizer: null,
+  OAuth: null,
+  UserId: null,
+  parent: null,
+  origin: null,
+  id: null,
+  init_token: null,
+  share: null
+};
+
+Neutron.auth_init = function (setkey, force_slow) {
+  if (setkey) {
+    gapi.client.setApiKey(GOOGLE_KEY);
+  }
+  
+  if (!Neutron.share) {
+    Neutron.share = new gapi.drive.share.ShareClient(GOOGLE_APP_ID);
+  }
+  
+  if (!Neutron.parent) {
+    setTimeout(Neutron.auth_init, 300);
+    return 0;
+  }
+  
+  if (!Neutron.gdrive_api_loaded) {
+    gapi.client.load('drive', 'v2', function () {
+      Neutron.gdrive_api_loaded = true;
+      Neutron.auth_init();
+    });
+    return 0;
+  }
+  
+  if (Neutron.authorizer) {
+    clearTimeout(Neutron.authorizer);
+    Neutron.authorizer = null;
+  }
+  
+  var options = {
+    client_id: GOOGLE_CLIENT_ID,
+    immediate: immediateAuth,
+    scope: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.scripts'
+    ]
+  };
+  
+  if (Neutron.UserId) {
+    options.user_id = Neutron.UserId;
+  }
+  
+  if (force_slow) {
+    options.immediate = false;
+  }
+  
+  gapi.auth.authorize(options, Neutron.auth_callback);
+};
+
+Neutron.auth_callback = function (OAuth) {
+  Neutron.parent.postMessage({'task': 'close-popup', id: Neutron.id}, Neutron.origin);
+  
+  if (OAuth && OAuth.error) {
+    Neutron.auth_init(false, true);
+    return null;
+  }
+  
+  if (OAuth && OAuth.expires_in) {
+    try {
+      gapi.drive.realtime.setServerAddress('https://docs.google.com/otservice/');
+    }
+    
+    catch (e) {
+      setTimeout(Neutron.auth_init, 300);
+      return 0;
+    }
+    
+    Neutron.OAuth = OAuth;
+    Neutron.authorizer = setTimeout(Neutron.auth_init, 60 * 25 * 1000);
+    Neutron.authorized = true;
+    
+    oauth = {
+      access_token: OAuth.access_token,
+      expires_at: OAuth.expires_at
+    };
+    
+    gapi.client.load('oauth2', 'v2', function() {
+      gapi.client.oauth2.userinfo.get().execute(function (resp) {
+        oauth.user_id = resp.id;
+        Neutron.UserId = resp.id;
+        
+        var request = gapi.client.drive.about.get();
+        request.execute(function (response) {
+          document.querySelector("#email").innerHTML = 'Account: ' + response.user.emailAddress;
+          immediateAuth = true;
+          Neutron.parent.postMessage({
+            task: 'token',
+            oauth: oauth,
+            email: response.user.emailAddress,
+            id: Neutron.id
+          }, Neutron.origin);
+        });
+      });
+    });
+  }
+};
+
+Neutron.pick_folder = function () {
+  var docsView = new google.picker.DocsView().
+    setIncludeFolders(true).
+    setMimeTypes('application/vnd.google-apps.folder').
+    setSelectFolderEnabled(true);
+  var picker = new google.picker.PickerBuilder().
+    addView(docsView).
+    setOAuthToken(Neutron.OAuth.access_token).
+    setDeveloperKey(GOOGLE_KEY).
+    setCallback(Neutron.pick_folder_callback).
+    build();
+  picker.setVisible(true);
+};
+
+Neutron.pick_folder_callback = function (data) {
+  if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+    var doc = data[google.picker.Response.DOCUMENTS][0];
+    var id = doc[google.picker.Document.ID];
+    Neutron.parent.postMessage({'task': 'folder-picked', id: Neutron.id, folderId: id}, Neutron.origin);
+  }
+  
+  else if (data[google.picker.Response.ACTION] == google.picker.Action.CANCEL) {
+    Neutron.parent.postMessage({'task': 'hide-webview', id: Neutron.id}, Neutron.origin);
+  }
+};
+
+Neutron.receive_message = function (event) {
+  if (event.data && event.data.task) {
+    if (event.data.task === 'handshake') {
+      if (!Neutron.parent) {
+        Neutron.parent = event.source;
+        Neutron.origin = event.origin;
+        Neutron.id = event.data.id;
+        
+        if (event.data.oauth) {
+          document.querySelector("#email").innerHTML = 'Initializing Account: ' + event.data.email;
+          gapi.load('auth:client,drive-realtime,drive-share,picker', function () {
+            gapi.auth.setToken({
+              access_token: event.data.oauth.access_token
+            });
+            
+            Neutron.UserId = event.data.oauth.user_id;
+            Neutron.auth_init(true);
+          });
+        }
+        
+        else {
+          gapi.load('auth:client,drive-realtime,drive-share,picker', function () {
+            Neutron.auth_init(true);
+          });
+        }
+      }
+    }
+    
+    else if (event.data.task === 'reauth') {
+      Neutron.auth_init(true);
+    }
+    
+    else if (event.data.task === 'pick-folder') {
+      Neutron.pick_folder();
+    }
+    
+    else if (Drive[event.data.task]) {
+      Drive[event.data.task](event.data, function (result) {
+        Neutron.parent.postMessage({
+          'task': event.data.task, id: Neutron.id, result: result, pid: event.data.pid
+        }, Neutron.origin);
+      });
+    }
+  }
+};
+
+window.addEventListener("message", Neutron.receive_message, false);
